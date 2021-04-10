16b37bd7ba6984def56b84e18d3476faa6b66392
xscapintime <louiesxscape@gmail.com>
2021-04-10 15:36:33 +0800

    new tastement

---
+++ note/new_taste.jl
@@ -0,0 +1,18 @@
+"""
+# Get some taste of Julia
+## 进入 Julia REPL 执行计算
+> REPL: read-eval-print loop
+> - means interactive envioment
+"""
+a = 5 * 8
+println(a)
+
+"""
+## 在 Julia REPL 外 (shell) 可直接执行
+利用 `-e` 参数实现
+
+`julia -e 'a = 5 * 8; println(a)`
+
+*python 有无此功能需要学习*
+"""
+
